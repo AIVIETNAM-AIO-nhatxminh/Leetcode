0b3453797c15d7b54ddfdc75f06562bd7d2d6471
nhatxminh <nhatxminh@gmail.com>
2026-07-25 20:51:50 +0700

    3536

--- python/3536.py
+++ python/3536.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
-        maxNum = 0
-        maxIdx = 0
-        result = 0
+        firstMax = 0
+        secondMax = 0
 
-        for idx, char in enumerate(str(n)):
-            if int(char) > maxNum:
-                maxNum = int(char)
-                maxIdx = idx
-        
-        for idx, char in enumerate(str(n)):
-            if idx == maxIdx:
-                continue
-            result = max(result, maxNum * int(char))
-        return result
+        for char in str(n):
+            if int(char) > firstMax:
+                secondMax = firstMax
+                firstMax = int(char)
+            elif int(char) > secondMax:
+                secondMax = int(char)
+
+        return firstMax * secondMax
